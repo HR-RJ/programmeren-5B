bce8184d1ed1c5ca6415d5fc8d341c1d06bc83be
RJ <90836237+HR-RJ@users.noreply.github.com>
2022-09-07 15:51:01 +0200

    added week 1 and some files to mess around in

---
+++ week1/les1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "les1.hpp"
+//definities, hoe iets werkt dus code met bijv objecten en variabelen
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << test << std::endl;
+    return 0;
+}
---
+++ week1/les1.hpp
@@ -0,0 +1,4 @@
+//declaraties geen echte code
+//header include je in de module die het gebruikt en de module waar hij bij hoort
+//header is een linking pin die ervoot zorgt dat de variabelen van code uniform zijn
+int test = 100;
